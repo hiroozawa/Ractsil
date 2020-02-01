4b4f1daeb32edb5565e59db9622cdebda9b9a37f
hiroozawa <aurelioozawa@gmail.com>
2020-02-01 11:11:56 -0300

    Enhanced error message

--- app/src/main/java/com/hiroozawa/ractsil/ui/MainActivity.kt
+++ app/src/main/java/com/hiroozawa/ractsil/ui/MainActivity.kt
@@ -2,31 +2,42 @@ package com.hiroozawa.ractsil.ui
 
 import android.os.Bundle
 import androidx.activity.viewModels
-import androidx.lifecycle.Observer
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.snackbar.Snackbar
 import com.hiroozawa.ractsil.R
+import com.hiroozawa.ractsil.databinding.ActivityMainBinding
+import com.hiroozawa.ractsil.ui.util.setupSnackbar
 import dagger.android.support.DaggerAppCompatActivity
 import javax.inject.Inject
 
 
-class MainActivity : DaggerAppCompatActivity(){
+class MainActivity : DaggerAppCompatActivity() {
 
     @Inject
     lateinit var viewModelFactory: ViewModelProvider.Factory
     private val viewModel by viewModels<MainActivityViewModel> { viewModelFactory }
 
+    private lateinit var viewDataBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        viewDataBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+        val navController = setupNavController()
+
+        with(viewDataBinding) {
+            navView.setupWithNavController(navController)
+            root.setupSnackbar(this@MainActivity, viewModel.errorLabel, Snackbar.LENGTH_LONG)
+        }
+    }
+
+    private fun setupNavController(): NavController {
         val navController = findNavController(R.id.nav_host_fragment)
         val appBarConfiguration = AppBarConfiguration(
             setOf(
@@ -34,10 +45,7 @@ class MainActivity : DaggerAppCompatActivity(){
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
-
-        viewModel.errorLabel.observe(this, Observer {
-
-        })
+        return navController
     }
 }
+
