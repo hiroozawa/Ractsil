d754e236440b9329ebf2abe5c1a869fd8c4f73ef
hiroozawa <aurelioozawa@gmail.com>
2020-04-12 19:01:47 -0300

    Convert DaggerBottomSheetFragment.kt to kotlin

---
+++ app/src/main/java/com/hiroozawa/ractsil/di/DaggerBottomSheetFragment.kt
@@ -0,0 +1,24 @@
+package com.hiroozawa.ractsil.di
+
+import android.content.Context
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import dagger.android.AndroidInjector
+import dagger.android.DispatchingAndroidInjector
+import dagger.android.HasAndroidInjector
+import dagger.android.support.AndroidSupportInjection
+import javax.inject.Inject
+
+abstract class DaggerBottomSheetFragment : BottomSheetDialogFragment(),
+    HasAndroidInjector {
+    @set:Inject
+    lateinit var androidInjector: DispatchingAndroidInjector<Any>
+
+    override fun onAttach(context: Context) {
+        AndroidSupportInjection.inject(this)
+        super.onAttach(context)
+    }
+
+    override fun androidInjector(): AndroidInjector<Any> {
+        return androidInjector
+    }
+}
